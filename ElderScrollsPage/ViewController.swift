b1025f8a347b37dd591fe129985d26b2691b3f48
Valtteri Ranta <valtterih.ranta@gmail.com>
2016-11-22 19:04:24 +0200

    Swiping with gesture recognizers

--- ElderScrollsPage/ViewController.swift
+++ ElderScrollsPage/ViewController.swift
@@ -10,18 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var controllerView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
     var images = [UIImageView]()
+    let lastPage = 2
+    let firstPage = 0
+    var currentPage = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+
+    override func viewDidAppear(_ animated: Bool) {
+        
+        var contentWidth: CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
+        
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
-            images.append(UIImageView(image: image))
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            contentWidth = scrollWidth * CGFloat(x + 1)
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+        }
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+
+    }
+    
+    @IBAction func detectSwipe(_ sender: UISwipeGestureRecognizer) {
+        if (currentPage < lastPage && sender.direction == UISwipeGestureRecognizerDirection.left) {
+            swipe(direction: 1)
+        }
+        if (currentPage > firstPage && sender.direction == UISwipeGestureRecognizerDirection.right) {
+            swipe(direction: -1)
         }
         
-        print("Count: \(images.count)")
     }
-
+    
+    func swipe(direction: Int) {
+        currentPage += direction
+        let point: CGPoint = CGPoint(x: scrollView.frame.size.width * CGFloat(currentPage), y: 0.0)
+        scrollView.setContentOffset(point, animated: true)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
